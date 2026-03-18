05114bdda65416c7d8b3228acf166e0e956f2c6d
genivaldofm34-ctrl <genivaldofm34@gmail.com>
2026-03-17 23:41:20 -0300

    Update meu_app.py

--- meu_app.py
+++ meu_app.py
@@ -5,34 +5,22 @@ st.set_page_config(page_title="PROFET PLAY", layout="centered")
 
 st.title("🎮 PROFET PLAY - SISTEMA ATIVO")
 
-# Conexão com Google Sheets
 conn = st.connection("gsheets", type=GSheetsConnection)
 
 try:
 
-    # lê a planilha
-    df = conn.read(
-        worksheet="PAINEL DE COMANDO",
-        ttl=0
-    )
+    df = conn.read(ttl=0)
 
-    st.subheader("📊 Painel de edição")
+    st.subheader("Painel de edição")
 
-    df_editado = st.data_editor(
-        df,
-        use_container_width=True
-    )
+    df_editado = st.data_editor(df, use_container_width=True)
 
-    if st.button("💾 SALVAR ALTERAÇÕES"):
+    if st.button("SALVAR ALTERAÇÕES"):
 
-        conn.update(
-            worksheet="PAINEL DE COMANDO",
-            data=df_editado
-        )
+        conn.update(data=df_editado)
 
-        st.success("Alterações salvas com sucesso!")
+        st.success("Dados salvos!")
         st.balloons()
 
 except Exception as e:
-
-    st.error(f"Erro ao carregar dados: {e}")
+    st.error(e)
