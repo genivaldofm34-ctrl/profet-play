55e33d8203a34543193a727fe2987268cd68f144
genivaldofm34-ctrl <genivaldofm34@gmail.com>
2026-03-17 23:29:47 -0300

    Update meu_app.py

--- meu_app.py
+++ meu_app.py
@@ -1,10 +1,38 @@
 import streamlit as st
 from streamlit_gsheets import GSheetsConnection
 
-st.title("Teste Google Sheets")
+st.set_page_config(page_title="PROFET PLAY", layout="centered")
 
+st.title("🎮 PROFET PLAY - SISTEMA ATIVO")
+
+# Conexão com Google Sheets
 conn = st.connection("gsheets", type=GSheetsConnection)
 
-df = conn.read()
+try:
 
-st.write(df)
+    # lê a planilha
+    df = conn.read(
+        worksheet="PAINEL DE COMANDO",
+        ttl=0
+    )
+
+    st.subheader("📊 Painel de edição")
+
+    df_editado = st.data_editor(
+        df,
+        use_container_width=True
+    )
+
+    if st.button("💾 SALVAR ALTERAÇÕES"):
+
+        conn.update(
+            worksheet="PAINEL DE COMANDO",
+            data=df_editado
+        )
+
+        st.success("Alterações salvas com sucesso!")
+        st.balloons()
+
+except Exception as e:
+
+    st.error(f"Erro ao carregar dados: {e}")
